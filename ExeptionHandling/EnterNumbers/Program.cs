4c577e230d2937ee1a11fd932d1b592a4b12449d
IvanMladenov <ismladenov@gmail.com>
2015-06-04 11:40:32 +0300

    Exeption handling

---
+++ ExeptionHandling/EnterNumbers/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EnterNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                try
+                {
+                    ReadNumber(1, 100);
+                }
+                catch(FormatException)
+                {
+                    Console.WriteLine("The number must be an integer!");
+                    i--;
+                }
+                catch(IndexOutOfRangeException)
+                {
+                    Console.WriteLine("The number must be in range [1...100]");
+                    i--;
+                }
+                
+            }
+           
+        }
+
+        static void ReadNumber(int start, int end)
+        {
+            int number = int.Parse(Console.ReadLine());
+
+            if(number<start||number>end)
+            {
+                throw new IndexOutOfRangeException();
+            }
+        }
+    }
+}
---
+++ ExeptionHandling/Sqrt/Sqrt.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sqrt
+{
+    class Sqrt
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                int number = int.Parse(Console.ReadLine());
+
+                if(number<0)
+                {
+                    throw new FormatException();
+                }
+
+                double sqrt = Math.Sqrt(number);
+
+                Console.WriteLine(sqrt);
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Iput must be an integer and greather than zero!");
+            }
+            finally
+            {
+                Console.WriteLine("Good bye!");
+            }
+        }
+    }
+}
