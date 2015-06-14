aa81e1baccca15289d244cf4ef4b2eff724f484f
IvanMladenov <ismladenov@gmail.com>
2015-06-14 21:47:31 +0300

    Add the last problem

---
+++ EncapsulationAndPolymorphism/04. Encapsulation-and-Polymorphism-Homework/TheSlum-Skeleton/Bonus.cs
@@ -0,0 +1,20 @@
+﻿using TheSlum.Items;
+
+namespace TheSlum
+{
+    using Interfaces;
+
+    public abstract class Bonus : Item, ITimeoutable
+    {
+        protected Bonus(string id, int healthEffect, int defenseEffect, int attackEffect)
+            : base(id, healthEffect, defenseEffect, attackEffect)
+        {
+        }
+
+        public int Timeout { get; set; }
+
+        public int Countdown { get; set; }
+
+        public bool HasTimedOut { get; set; }
+    }
+}
---
+++ EncapsulationAndPolymorphism/04. Encapsulation-and-Polymorphism-Homework/TheSlum-Skeleton/Healer.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using TheSlum.Interfaces;
+using TheSlum.Items;
+
+namespace TheSlum
+{
+    class Healer:Character,IHeal
+    {
+        private const int DefaultHealthPoints = 75;
+        private const int DefaultDefensePoints = 50;
+        private const int DefaultRange = 6;
+
+        private int healingPoints = 60;
+
+        public Healer(string id, int x, int y, Team team) 
+            : base(id, x, y, DefaultHealthPoints, DefaultDefensePoints, team, DefaultRange)
+        {
+        }
+
+        public int HealingPoints
+        {
+            get {return this.healingPoints ; }
+            set { this.healingPoints = value; }
+        }
+
+        public override Character GetTarget(IEnumerable<Character> targetsList)
+        {
+            Character target =
+                targetsList.Where(x => x.IsAlive)
+                    .Where(x => x.Team == this.Team)
+                    .Where(x => x != this)
+                    .OrderBy(x => x.HealthPoints)
+                    .FirstOrDefault();
+
+            return target;
+        }
+
+        public override void AddToInventory(Item item)
+        {
+            this.Inventory.Add(item);
+            this.ApplyItemEffects(item);
+        }
+
+        public override void RemoveFromInventory(Item item)
+        {
+            this.Inventory.Remove(item);
+            this.RemoveItemEffects(item);
+        }
+
+        public override string ToString()
+        {
+            return String.Format("{0}, Healing:{1}", base.ToString(), this.healingPoints);
+        }
+    }
+}
